1f4926a7a2bec82ef7b4326d26d4487bf3402f84
Martin Etchebarne <martin@etchebarne.net>
2026-04-15 23:43:14 -0300

    fix: prevent middle-click paste in editor

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 document.addEventListener("contextmenu", (e) => e.preventDefault());
+document.addEventListener("auxclick", (e) => {
+  if (e.button === 1) e.preventDefault();
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
